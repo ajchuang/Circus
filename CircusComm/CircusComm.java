7c5dad2bbd5a75e1bc05ee3d8dc1df6f00f5ac6e
ajchuang <alfred.jenchieh.huang@gmail.com>
2014-11-25 09:47:24 -0500

    update CircusComm

--- CircusComm/CircusComm.java
+++ CircusComm/CircusComm.java
@@ -7,6 +7,34 @@ public class CircusComm {
         System.out.println ("[CircusComm] " + s);
     }
     
+    /* general functions */
+    public static void txSysUp (int swId, int swType, ObjectOutputStream oos) {
+    }
+    
+    public static void txSysDown (int swId, ObjectOutputStream oos) {
+    }
+    
+    /* CS functions */
+    public static void txSetup_cs (int swId, int swType, ObjectOutputStream oos) {
+    }
+    
+    public static void txReconfig_cs (int swId, int swType, ObjectOutputStream oos) {
+    }
+    
+    public static void txTeardown_cs (int swId, int swType, ObjectOutputStream oos) {
+    }
+    
+    /* PS functions */
+    public static void txAddEntry_ps (int swId, int swType, ObjectOutputStream oos) {
+    }
+    
+    public static void txModifyEntry_ps (int swId, int swType, ObjectOutputStream oos) {
+    }
+    
+    public static void txRmEntry_ps (int swId, int swType, ObjectOutputStream oos) {
+    }
+    
+    
     public static void transmit (CircusCommObj cco, ObjectOutputStream oos) {
         try {
             oos.writeObject (cco);
