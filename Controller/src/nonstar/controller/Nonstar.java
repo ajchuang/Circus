a7e743af92f6f81de4431d383d6a9f85f901ad6e
Liyuan Zheng <lz2375@columbia.edu>
2014-12-08 17:31:51 -0500

    testing

--- Controller/src/nonstar/controller/Nonstar.java
+++ Controller/src/nonstar/controller/Nonstar.java
@@ -8,14 +8,54 @@ import nonstar.basic.Switch;
 
 public class Nonstar extends NonstarBase {
 
-	boolean flag = true;
+	HashMap<Switch,Integer> srcCount = new HashMap<Switch,Integer>();
+	HashMap<Switch,Integer> dstCount = new HashMap<Switch,Integer>();
 	public void on_start()  {
-		
+		int i = 0;
+
+	}
+	public int getSwitchCount(Switch sw)  {
+		int res = 0;
+		if(srcCount.containsKey(sw)) {
+			res = res + srcCount.get(sw);
+
+		}
+
+		if(dstCount.containsKey(sw)) {
+			res = res + dstCount.get(sw);
+
+		}
+
+		return res;
+
+	}
+	public void incrementSwichCount(HashMap<Switch,Integer> map, Switch sw)  {
+		if(map.containsKey(sw)) {
+			map.put(sw, 1);
+
+		}
+
+		else {
+			map.put(sw, map.get(sw) + 1);
+
+		}
+
+
 	}
 	public Flow on_req(Switch src, Switch dst)  {
 		Flow f = getCurrCircuit(src, dst);
 		if(f == null) {
+			if(getSwitchCount(src) < 3 || getSwitchCount(dst) < 3) {
 			f = setupCircuit(src, dst);
+
+		}
+
+
+		}
+
+		if(f != null) {
+			incrementSwichCount(srcCount, src);
+		incrementSwichCount(dstCount, dst);
 
 		}
 
